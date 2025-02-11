77a543dec770905a1845c1147cc2d6d583efe894
Gourav <gouravkumarat@gmail.com>
2025-02-11 12:13:24 +0530

    fixed prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
 	plugins: [tailwindcss()],
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true,
+				}),
+			],
+		},
+	},
+	define: {
+		global: "window", // Make global available for browser
+	},
 });
